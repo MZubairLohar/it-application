f87bd758ee38cfec0e082855cb92b3ca78e22e04
MZubairLohar <zubairgituser@gmail.com>
2020-06-10 22:56:36 +0500

    first run

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Fragment, useEffect} from 'react';
 import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
-  useEffect (()=>{
+  useEffect (() => {
     //init initializes materializes js
     M.AutoInit();
   });
@@ -13,9 +14,12 @@ const App = () => {
     
      <Fragment>
        <SearchBar />
+       <div className='container'>
+         <Logs />
+         </div>
      </Fragment>
     
   );
-}
+};
 
 export default App;
