21b22fcda31b98bf982797cef23073ee6e6afe95
MZubairLohar <zubairgituser@gmail.com>
2020-06-13 00:15:36 +0500

    preloader & delete button

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -10,13 +10,19 @@ const LogItem = ( { log } ) => {
     <a href='#edit-log-model' className={`model-trigger ${log.attention ? 'red-text': 'blue-text'}`}>{log.message}</a>
         <br />
     <span className='grey-text'>
-    <span className='black-text'>ID #{log.id} </span> last updated by {''}
-        <span className='black-text'>{log.tech }</span> on <moment format= ' do mmmm yyyy, h:mm:ss'>{ log.date}</moment>
+        <span className='black-text'>ID #{log.id} </span> last updated by {' '}
+        <span className='black-text'>{log.tech }</span> on {' '}
+        <Moment format= ' do MMMM yyyy, h:mm:ss   a'>{ log.date}</Moment>
     </span>
+    <a href="#!" className="secendary-content  ">
+          <div align="right">
+        <i className="   material-icons  grey-text" >   delete   </i>
+        </div>
+    </a>
         </div>
         </li>
-    )
-}
+    );
+};
 LogItem.prpTypes = {
     log: PropTypes.object.isRequired
 }
